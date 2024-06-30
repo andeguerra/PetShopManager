9c8b1ff7d85039e8022c80346c68ce1cf1f3d45d
jtSUCKA <joaotneto04@gmail.com>
2024-06-30 16:46:33 -0300

    Tela de Login Pronta

---
+++ ProjetoDesktop/FrmLogin.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ProjetoDesktop
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnSair = new RoundedButton();
+            this.btnEntrar = new RoundedButton();
+            this.txtSenha = new RoundTextBox();
+            this.txtUsuario = new RoundTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.White;
+            this.btnSair.CornerRadius = 10;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.btnSair.Location = new System.Drawing.Point(780, 687);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(181, 53);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.roundedButton2_Click);
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(192)))), ((int)(((byte)(214)))));
+            this.btnEntrar.CornerRadius = 10;
+            this.btnEntrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEntrar.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.btnEntrar.Location = new System.Drawing.Point(972, 687);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(181, 53);
+            this.btnEntrar.TabIndex = 2;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = false;
+            this.btnEntrar.Click += new System.EventHandler(this.roundedButton1_Click);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.CornerRadius = 10;
+            this.txtSenha.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(780, 449);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(373, 35);
+            this.txtSenha.TabIndex = 1;
+            this.txtSenha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSenha.UseSystemPasswordChar = true;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.CornerRadius = 10;
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(780, 362);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(373, 35);
+            this.txtUsuario.TabIndex = 0;
+            this.txtUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtUsuario.TextChanged += new System.EventHandler(this.roundTextBox1_TextChanged_2);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1280, 768);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtUsuario);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "FrmLogin";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmLogin";
+            this.Load += new System.EventHandler(this.FrmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private RoundTextBox txtUsuario;
+        private RoundTextBox txtSenha;
+        private RoundedButton btnEntrar;
+        private RoundedButton btnSair;
+    }
+}
---
+++ ProjetoDesktop/RoundTextBox.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class RoundTextBox : TextBox
+{
+    private int cornerRadius = 10; // Raio dos cantos
+
+    public int CornerRadius
+    {
+        get { return cornerRadius; }
+        set
+        {
+            cornerRadius = value;
+            this.Invalidate(); // Redesenha o controle quando o raio é alterado
+        }
+    }
+
+    protected override void OnResize(EventArgs e)
+    {
+        base.OnResize(e);
+        UpdateRegion(); // Atualiza a região quando o controle é redimensionado
+    }
+
+    protected override void OnSizeChanged(EventArgs e)
+    {
+        base.OnSizeChanged(e);
+        UpdateRegion(); // Atualiza a região quando o tamanho do controle é alterado
+    }
+
+    private void UpdateRegion()
+    {
+        GraphicsPath path = new GraphicsPath();
+        int diameter = cornerRadius * 2;
+
+        // Cria um caminho com cantos arredondados
+        path.AddArc(0, 0, diameter, diameter, 180, 90); // Canto superior esquerdo
+        path.AddArc(Width - diameter, 0, diameter, diameter, 270, 90); // Canto superior direito
+        path.AddArc(Width - diameter, Height - diameter, diameter, diameter, 0, 90); // Canto inferior direito
+        path.AddArc(0, Height - diameter, diameter, diameter, 90, 90); // Canto inferior esquerdo
+        path.CloseFigure();
+
+        this.Region = new Region(path); // Define a região para cliques corretamente moldada
+    }
+}
