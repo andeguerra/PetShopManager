7f3b8bd539efd3f20204490da83357c8e4646f23
andeguerra <anderson.dearaujo@outlook.com>
2024-07-02 05:27:27 -0300

    adicionando banco de dados

---
+++ Banco de Dados/db-petshop.sql
@@ -0,0 +1,626 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 02/07/2024 às 10:26
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db-petshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `agendamentos`
+--
+
+CREATE TABLE `agendamentos` (
+  `agendamento_id` int(11) NOT NULL,
+  `data` datetime DEFAULT NULL,
+  `pet_id` int(11) DEFAULT NULL,
+  `servico_id` int(11) DEFAULT NULL,
+  `funcionario_id` int(11) DEFAULT NULL,
+  `observacoes` varchar(255) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `cliente_id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `endereco_id` int(11) DEFAULT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `fidelidade_pontos` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `contas_pagar_receber`
+--
+
+CREATE TABLE `contas_pagar_receber` (
+  `conta_id` int(11) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `data_vencimento` datetime DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `tipo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `descontos`
+--
+
+CREATE TABLE `descontos` (
+  `desconto_id` int(11) NOT NULL,
+  `nome_desconto` varchar(40) DEFAULT NULL,
+  `desc_desconto` varchar(400) DEFAULT NULL,
+  `valor_desconto` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `endereco`
+--
+
+CREATE TABLE `endereco` (
+  `endereco_id` int(11) NOT NULL,
+  `cep` varchar(10) NOT NULL,
+  `uf` char(2) DEFAULT NULL COMMENT 'Estado (UF), valores válidos: AC, AL, AP, AM, BA, CE, DF, ES, GO, MA, MT, MS, MG, PA, PB, PR, PE, PI, RJ, RN, RS, RO, RR, SC, SP, SE, TO',
+  `cidade` varchar(50) DEFAULT NULL,
+  `bairro` varchar(70) DEFAULT NULL,
+  `logradouro` varchar(150) DEFAULT NULL,
+  `num` varchar(10) DEFAULT NULL,
+  `compl1` varchar(50) DEFAULT NULL,
+  `compl2` varchar(50) DEFAULT NULL,
+  `obs` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `especie_pet`
+--
+
+CREATE TABLE `especie_pet` (
+  `especie_id` int(11) NOT NULL,
+  `especie` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `fornecedores`
+--
+
+CREATE TABLE `fornecedores` (
+  `fornecedor_id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `contato` varchar(100) DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `funcionarios`
+--
+
+CREATE TABLE `funcionarios` (
+  `funcionario_id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `cargo` varchar(100) DEFAULT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `itens_venda`
+--
+
+CREATE TABLE `itens_venda` (
+  `item_id` int(11) NOT NULL,
+  `venda_id` int(11) DEFAULT NULL,
+  `produto_id` int(11) DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `preco_unitario` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `movimentacao_estoque`
+--
+
+CREATE TABLE `movimentacao_estoque` (
+  `movimentacao_id` int(11) NOT NULL,
+  `produto_id` int(11) DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `tipo` varchar(50) DEFAULT NULL,
+  `data` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `nivel_usuario`
+--
+
+CREATE TABLE `nivel_usuario` (
+  `id` int(11) NOT NULL,
+  `nivel` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `nivel_usuario`
+--
+
+INSERT INTO `nivel_usuario` (`id`, `nivel`) VALUES
+(1, 'Administra'),
+(2, 'Usuário');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pets`
+--
+
+CREATE TABLE `pets` (
+  `pet_id` int(11) NOT NULL,
+  `num_uni` int(11) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `especie_id` int(11) DEFAULT NULL,
+  `raca_id` int(11) DEFAULT NULL,
+  `sexo_id` int(11) DEFAULT NULL,
+  `idade` int(11) DEFAULT NULL,
+  `cor` varchar(20) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `cliente_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `produto_id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `preco` decimal(10,2) DEFAULT NULL,
+  `quantidade_estoque` int(11) DEFAULT NULL,
+  `fornecedor_id` int(11) DEFAULT NULL,
+  `validade` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `receitas_despesas`
+--
+
+CREATE TABLE `receitas_despesas` (
+  `registro_id` int(11) NOT NULL,
+  `data` datetime DEFAULT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `tipo` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `relatorios`
+--
+
+CREATE TABLE `relatorios` (
+  `relatorio_id` int(11) NOT NULL,
+  `tipo` varchar(50) DEFAULT NULL,
+  `data_geracao` datetime DEFAULT NULL,
+  `conteudo` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `servicos`
+--
+
+CREATE TABLE `servicos` (
+  `servico_id` int(11) NOT NULL,
+  `descricao` varchar(255) DEFAULT NULL,
+  `preco` decimal(10,2) DEFAULT NULL,
+  `duracao` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `sexo_pet`
+--
+
+CREATE TABLE `sexo_pet` (
+  `sexo_id` int(11) NOT NULL,
+  `sexo` varchar(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario_id` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `senha` varchar(255) DEFAULT NULL,
+  `nivel_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`usuario_id`, `nome`, `email`, `senha`, `nivel_id`) VALUES
+(3, 'Ana', 'ana@adm.com', 'adm123', 1),
+(4, 'Anderson', 'anderson@usuario.com', '123456', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `vendas`
+--
+
+CREATE TABLE `vendas` (
+  `venda_id` int(11) NOT NULL,
+  `data` datetime DEFAULT NULL,
+  `cliente_id` int(11) DEFAULT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `total` decimal(10,2) DEFAULT NULL,
+  `desconto_id` int(11) DEFAULT NULL,
+  `forma_pagamento` varchar(50) DEFAULT NULL,
+  `status_pagamento` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `agendamentos`
+--
+ALTER TABLE `agendamentos`
+  ADD PRIMARY KEY (`agendamento_id`),
+  ADD KEY `pet_id` (`pet_id`),
+  ADD KEY `servico_id` (`servico_id`),
+  ADD KEY `funcionario_id` (`funcionario_id`);
+
+--
+-- Índices de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`cliente_id`),
+  ADD KEY `endereco_id` (`endereco_id`);
+
+--
+-- Índices de tabela `contas_pagar_receber`
+--
+ALTER TABLE `contas_pagar_receber`
+  ADD PRIMARY KEY (`conta_id`);
+
+--
+-- Índices de tabela `descontos`
+--
+ALTER TABLE `descontos`
+  ADD PRIMARY KEY (`desconto_id`);
+
+--
+-- Índices de tabela `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`endereco_id`);
+
+--
+-- Índices de tabela `especie_pet`
+--
+ALTER TABLE `especie_pet`
+  ADD PRIMARY KEY (`especie_id`);
+
+--
+-- Índices de tabela `fornecedores`
+--
+ALTER TABLE `fornecedores`
+  ADD PRIMARY KEY (`fornecedor_id`);
+
+--
+-- Índices de tabela `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  ADD PRIMARY KEY (`funcionario_id`);
+
+--
+-- Índices de tabela `itens_venda`
+--
+ALTER TABLE `itens_venda`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `venda_id` (`venda_id`),
+  ADD KEY `produto_id` (`produto_id`);
+
+--
+-- Índices de tabela `movimentacao_estoque`
+--
+ALTER TABLE `movimentacao_estoque`
+  ADD PRIMARY KEY (`movimentacao_id`),
+  ADD KEY `produto_id` (`produto_id`);
+
+--
+-- Índices de tabela `nivel_usuario`
+--
+ALTER TABLE `nivel_usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `pets`
+--
+ALTER TABLE `pets`
+  ADD PRIMARY KEY (`pet_id`),
+  ADD KEY `especie_id` (`especie_id`),
+  ADD KEY `sexo_id` (`sexo_id`),
+  ADD KEY `cliente_id` (`cliente_id`);
+
+--
+-- Índices de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`produto_id`),
+  ADD KEY `fornecedor_id` (`fornecedor_id`);
+
+--
+-- Índices de tabela `receitas_despesas`
+--
+ALTER TABLE `receitas_despesas`
+  ADD PRIMARY KEY (`registro_id`);
+
+--
+-- Índices de tabela `relatorios`
+--
+ALTER TABLE `relatorios`
+  ADD PRIMARY KEY (`relatorio_id`);
+
+--
+-- Índices de tabela `servicos`
+--
+ALTER TABLE `servicos`
+  ADD PRIMARY KEY (`servico_id`);
+
+--
+-- Índices de tabela `sexo_pet`
+--
+ALTER TABLE `sexo_pet`
+  ADD PRIMARY KEY (`sexo_id`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD KEY `fk_nivel` (`nivel_id`);
+
+--
+-- Índices de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  ADD PRIMARY KEY (`venda_id`),
+  ADD KEY `cliente_id` (`cliente_id`),
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `desconto_id` (`desconto_id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `agendamentos`
+--
+ALTER TABLE `agendamentos`
+  MODIFY `agendamento_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `contas_pagar_receber`
+--
+ALTER TABLE `contas_pagar_receber`
+  MODIFY `conta_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `descontos`
+--
+ALTER TABLE `descontos`
+  MODIFY `desconto_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `endereco_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `especie_pet`
+--
+ALTER TABLE `especie_pet`
+  MODIFY `especie_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `fornecedores`
+--
+ALTER TABLE `fornecedores`
+  MODIFY `fornecedor_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  MODIFY `funcionario_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `itens_venda`
+--
+ALTER TABLE `itens_venda`
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `movimentacao_estoque`
+--
+ALTER TABLE `movimentacao_estoque`
+  MODIFY `movimentacao_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `nivel_usuario`
+--
+ALTER TABLE `nivel_usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `pets`
+--
+ALTER TABLE `pets`
+  MODIFY `pet_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `produto_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `receitas_despesas`
+--
+ALTER TABLE `receitas_despesas`
+  MODIFY `registro_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `relatorios`
+--
+ALTER TABLE `relatorios`
+  MODIFY `relatorio_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `servicos`
+--
+ALTER TABLE `servicos`
+  MODIFY `servico_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `sexo_pet`
+--
+ALTER TABLE `sexo_pet`
+  MODIFY `sexo_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `vendas`
+--
+ALTER TABLE `vendas`
+  MODIFY `venda_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `agendamentos`
+--
+ALTER TABLE `agendamentos`
+  ADD CONSTRAINT `agendamentos_ibfk_1` FOREIGN KEY (`pet_id`) REFERENCES `pets` (`pet_id`),
+  ADD CONSTRAINT `agendamentos_ibfk_2` FOREIGN KEY (`servico_id`) REFERENCES `servicos` (`servico_id`),
+  ADD CONSTRAINT `agendamentos_ibfk_3` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionarios` (`funcionario_id`);
+
+--
+-- Restrições para tabelas `clientes`
+--
+ALTER TABLE `clientes`
+  ADD CONSTRAINT `clientes_ibfk_1` FOREIGN KEY (`endereco_id`) REFERENCES `endereco` (`endereco_id`);
+
+--
+-- Restrições para tabelas `itens_venda`
+--
+ALTER TABLE `itens_venda`
+  ADD CONSTRAINT `itens_venda_ibfk_1` FOREIGN KEY (`venda_id`) REFERENCES `vendas` (`venda_id`),
+  ADD CONSTRAINT `itens_venda_ibfk_2` FOREIGN KEY (`produto_id`) REFERENCES `produtos` (`produto_id`);
+
+--
+-- Restrições para tabelas `movimentacao_estoque`
+--
+ALTER TABLE `movimentacao_estoque`
+  ADD CONSTRAINT `movimentacao_estoque_ibfk_1` FOREIGN KEY (`produto_id`) REFERENCES `produtos` (`produto_id`);
+
+--
+-- Restrições para tabelas `pets`
+--
+ALTER TABLE `pets`
+  ADD CONSTRAINT `pets_ibfk_1` FOREIGN KEY (`especie_id`) REFERENCES `especie_pet` (`especie_id`),
+  ADD CONSTRAINT `pets_ibfk_2` FOREIGN KEY (`sexo_id`) REFERENCES `sexo_pet` (`sexo_id`),
+  ADD CONSTRAINT `pets_ibfk_3` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`cliente_id`);
+
+--
+-- Restrições para tabelas `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `produtos_ibfk_1` FOREIGN KEY (`fornecedor_id`) REFERENCES `fornecedores` (`fornecedor_id`);
+
+--
+-- Restrições para tabelas `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `fk_nivel` FOREIGN KEY (`nivel_id`) REFERENCES `nivel_usuario` (`id`);
+
+--
+-- Restrições para tabelas `vendas`
+--
+ALTER TABLE `vendas`
+  ADD CONSTRAINT `vendas_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `clientes` (`cliente_id`),
+  ADD CONSTRAINT `vendas_ibfk_2` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`),
+  ADD CONSTRAINT `vendas_ibfk_3` FOREIGN KEY (`desconto_id`) REFERENCES `descontos` (`desconto_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
